8d8dd75191b825e6404b830e61fa04375348d1cd
Bence Lukacsy <bence.lukacsy@gmail.com>
2023-05-19 01:13:05 -0400

    incomplete packages, these are only from openai

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from picamera2 import Picamera2
+from gpiozero import Button
+from time import sleep
+import openai
+
+camera = Picamera2()
+
+config = camera.create_still_configuration(raw={"size": camera.sensor_resolution})
+camera.configure(config)
+
+camera.start()
+sleep(5)
+
+# image = camera.capture_image() // for API
+camera.capture_file("/home/bence/image.png")
+
+camera.close()
