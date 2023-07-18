a213e30d2b92985cd697cd84d276b0ec35a42589
Bence Lukacsy <bence.lukacsy@gmail.com>
2023-07-18 18:52:43 -0400

    utilizing bard instead of openai

--- main.py
+++ main.py
@@ -1,49 +1,59 @@
 from picamera2 import Picamera2
 from gpiozero import Button
 from time import sleep
-import openai
 import json
+from bardapi import Bard
+# import openai
 
 with open('keys.json','r') as file:
     key = json.load(file)
 
-openai.api_key = key['OpenAI_API_Key']
+# openai_key = key['OpenAI_API_Key']
+bard_key = key['Bard_Cookie_Value']
+
+# openai.api_key = openai_key
+bard = Bard(token=bard_key)
 
 camera = Picamera2()
 config = camera.create_still_configuration(raw={"size": camera.sensor_resolution})
 camera.configure(config)
 
-button = Button(25) # GPIO25
+button = Button(25)
 
 def take_picture():
     camera.start()
     sleep(3)
-
-    image = camera.capture_image() # returns a PIL image
-    # camera.capture_file('image.jpg') --> can open file in binary and send to OpenAI, if easier
-
+    camera.capture_file('image.jpg')
     camera.stop()
-    return image
 
-# POST request
 def send_picture(image):
-
-    # Waiting for image upload feature
+    
+    # OpenAI API Request
+    
+    '''
     completion = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "user", "content": "Tell me a short joke"}
+            {"role": "user", "content": "Who are you?"}
         ]
     )
 
     return completion.choices[0].message['content']
+    '''
+    
+    # Unofficial Google Bard API Request
+    
+    bard_answer = bard.ask_about_image('Describe the contents of this image', image)
+    return bard_answer['content']
 
 while True:
     try:
         button.wait_for_press()
-        image = take_picture()
+        take_picture()
+        sleep(1)
+        image = open('image.jpg', 'rb').read()
         response = send_picture(image)
-        print(response) # can email the response, play it through a speaker, etc.
+        print(response)
     except:
         camera.stop()
         break
